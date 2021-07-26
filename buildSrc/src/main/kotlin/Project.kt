f54db396b213ed3c5d9cded6e9970c346a50b4e2
Arunkumar <arunkumar9t2@users.noreply.github.com>
2021-07-26 23:15:34 +0800

    Fix copyright getting duplicated on Kotlin files
    Signed-off-by: Arunkumar <arunkumar9t2@users.noreply.github.com>

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -20,22 +20,6 @@ import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
 
-/*
- * Copyright 2021 Arunkumar
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 open class ConfigurablePlugin(private val configuration: Project.() -> Unit) : Plugin<Project> {
   override fun apply(project: Project): Unit = configuration(project)
 }
